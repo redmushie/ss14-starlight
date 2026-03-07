4b125fb7fe9f09a28c3eb32fe30413aaf0304732
Red Mushie <noreply@redmushie.me>
2026-03-07 13:52:17 +0100

    Make death rattles include coordinates

--- Content.Server/Trigger/Systems/RattleOnTriggerSystem.cs
+++ Content.Server/Trigger/Systems/RattleOnTriggerSystem.cs
@@ -13,6 +13,7 @@ public sealed class RattleOnTriggerSystem : EntitySystem
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
     [Dependency] private readonly RadioSystem _radio = default!;
     [Dependency] private readonly NavMapSystem _navMap = default!;
+    [Dependency] private readonly SharedTransformSystem _transform = default!; // Starlight
 
     public override void Initialize()
     {
@@ -41,6 +42,17 @@ public sealed class RattleOnTriggerSystem : EntitySystem
 
         // Gets the location of the user
         var posText = FormattedMessage.RemoveMarkupOrThrow(_navMap.GetNearestBeaconString(target.Value));
+        
+        // Starlight BEGIN
+        if (TryComp(ent.Owner, out TransformComponent? transComp))
+        {
+            // Append coordinates to rattle if possible.
+            var pos = _transform.GetMapCoordinates(ent.Owner, xform: transComp);
+            var x = (int)pos.X;
+            var y = (int)pos.Y;
+            posText += $" ({x}, {y})";
+        }
+        // Starlight END
 
         var message = Loc.GetString(messageId, ("user", target.Value), ("position", posText));
         // Sends a message to the radio channel specified by the implant
